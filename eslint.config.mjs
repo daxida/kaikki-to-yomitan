d29b5345dcc50471179aca2a37ad56871b47dbf8
daxida <galmaw@hotmail.com>
2025-10-13 22:01:29 +0200

    Setup minimal `npm run lint` command with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: {
+            globals: globals.node,
+        },
+        rules: {
+            indent: ["error", 4],
+            "no-trailing-spaces": "error",
+            "eol-last": ["error", "always"],
+        },
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "script",
+        },
+    },
+]);
